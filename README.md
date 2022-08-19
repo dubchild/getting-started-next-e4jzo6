c5a8c8ad485d1b287b567237c12e449853ad65d2
dubchild <dubchild@gmail.com>
2022-08-19 01:58:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# getting-started-next-e4jzo6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/getting-started-next-e4jzo6)
---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import '../styles/tailwind.css';
+import { storyblokInit, apiPlugin } from '@storyblok/react';
+import Feature from '../components/Feature';
+import Grid from '../components/Grid';
+import Page from '../components/Page';
+import Teaser from '../components/Teaser';
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: '3RWTWDI7BlPUEeI3gy5d2gtt',
+  use: [apiPlugin],
+  components,
+  apiOptions: {
+    region: "us" // region code here
+  }
+});
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
