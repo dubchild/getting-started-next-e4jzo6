2cc7b6b50bb00aa45362116ef2f6019b2e82ea77
dubchild <dubchild@gmail.com>
2022-08-19 09:47:05 -0400

    2 files modified

--- components/Quote.js
+++ components/Quote.js
@@ -2,8 +2,9 @@ import { storyblokEditable } from '@storyblok/react';
 
 const Quote = ({ blok }) => {
   return (
-    <div className="py-8 mb-6" {...storyblokEditable(blok)} key={blok._uid}>
-      <h2 className="text-5xl font-bold text-center">" {blok.headline} "</h2>
+    <div className="py-8 mb-6 bg-slate-100" {...storyblokEditable(blok)} key={blok._uid}>
+      <h2 className="text-5xl font-bold text-center text-slate-500">" {blok.headline} "</h2>
+      <h2 className="text-2xl font-bold text-center ">-{blok.quoteme}</h2>
     </div>
   );
 };
--- components/Teaser.js
+++ components/Teaser.js
@@ -4,9 +4,7 @@ const Teaser = ({ blok }) => {
   return (
     <div className="py-8 mb-6" {...storyblokEditable(blok)} key={blok._uid}>
       <h2 className="text-5xl font-bold text-center"> {blok.headline} </h2>
-      <h3 className="text-2xl font-bold text-center"> {blok.quoteme} </h3>
     </div>
-    
   );
 };
 
